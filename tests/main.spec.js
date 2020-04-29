87498ed75261ce7f7705b8bcd5cb8c80e89c0c3b
Davi Mattos <davimattos@Davis-MacBook-Pro.local>
2020-04-29 19:09:19 -0300

    create smoke tests

---
+++ tests/main.spec.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import {
+  search,
+  searchAlbuns,
+  searchArtists,
+  searchPlaylists,
+  searchTracks,
+} from "../src/main";
+
+describe("Spotify Wrapper", () => {
+  describe("smoke tests", () => {
+    it("should exist the search method", () => {
+      expect(search).to.exist;
+    });
+
+    it("should exist the searchAlbuns method", () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it("should exist the searchArtists method", () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it("should exist the searchTracks method", () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it("should exist the searchPlaylists method", () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+});
