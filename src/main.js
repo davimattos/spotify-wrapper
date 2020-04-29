c3d3476aeff1cfff71feace4f81863be5d024c34
Davi Mattos <davimattos@Davis-MacBook-Pro.local>
2020-04-29 20:12:58 -0300

    implements a generic search request

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-export const search = () => {};
+export const search = (query, type) => {
+  fetch(`https://api.spotify.com/v1/search?query=${query}&type=${type}`);
+};
+
 export const searchAlbuns = () => {};
 export const searchArtists = () => {};
 export const searchTracks = () => {};
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,13 @@
-import { expect } from "chai";
+import chai, { expect } from "chai";
+import sinon from "sinon";
+import sinonChai from "sinon-chai";
+import sinonStubPromise from "sinon-stub-promise";
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require("node-fetch");
+
 import {
   search,
   searchAlbuns,
@@ -27,6 +36,25 @@ describe("Spotify Wrapper", () => {
 
     it("should exist the searchPlaylists method", () => {
       expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe("Generic search", () => {
+    it("should call the fetch function", () => {
+      const fetchedStub = sinon.stub(global, "fetch");
+      const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+      fetchedStub.restore();
+    });
+
+    it("should receive the correct url to fetch", () => {
+      const fetchedStub = sinon.stub(global, "fetch");
+
+      const artists = search("tania", "artist");
+      expect(fetchedStub).to.have.been.calledWith(
+        "https://api.spotify.com/v1/search?query=tania&type=artist"
+      );
     });
   });
 });
